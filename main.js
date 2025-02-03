4bf52f5ce7b7d828262aff1bae9f90b487c7b713
chakshu-ad <2022bcachakshu12880@poornima.edu.in>
2025-02-03 11:13:16 +0530

    new code

--- main.js
+++ main.js
@@ -1,10 +1,41 @@
-let firstname = "chakshu";
-let ismarried = false;
-let age = 25;
+// let firstname = "chakshu";
+// let ismarried = false;
+// let age = 25;
 
-if (ismarried == true) {
-    console.log( firstname +  " are married");
-}
-else{
-    console.log( firstname +  " are not married");
+// if (ismarried == true) {
+//     console.log( firstname +  " are married");
+// }
+// else{
+//     console.log( firstname +  " are not married");
+// }
+
+// let answer= 0;
+// for(let i=0; i<=1000; i = i + 1){
+//     answer = answer + i;
+// }
+// console.log(answer);
+
+// let firstname = "chakshu";
+// let lastname = "adlakha";
+
+// console.log("Hello Mr. " + firstname +  lastname);
+
+// let firstname = "chakshu";
+// let isgender = "male";
+
+// if(isgender == "male"){
+//     console.log("hello Mr. " + firstname);
+// }
+// else{
+//     console.log("I don't know who you are.")
+// }
+
+
+const ages = [22, 23, 24, 25, 26];
+const noofpeople = ages.length; // Declare noofpeople with const or let
+
+for (let i = 0; i < noofpeople; i++) {
+  if (ages[i] % 2 == 0) {
+    console.log(ages[i]);
+  }
 }
