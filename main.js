d93ecd69f3d92877c0b941fc9eb63eed41d489d6
chakshu-ad <2022bcachakshu12880@poornima.edu.in>
2025-02-04 23:54:13 +0530

    new code

--- main.js
+++ main.js
@@ -40,3 +40,12 @@
 //   }
 // }
 
+const numbers = [22, 50, 55, 200, 1999];
+let largest = numbers[0];
+
+for(let i = 1; i < numbers.length; i++){
+    if(numbers[i] > largest){
+        largest = numbers[i];
+    }
+}
+console.log(largest);
